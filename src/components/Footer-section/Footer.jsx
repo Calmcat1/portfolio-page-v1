84a6b7a042a12f123fc1269d509743b3ee33689f
Lee <lgitonga101@gmail.com>
2024-01-11 12:50:06 +0300

    modified:   src/components/Footer-section/Footer.jsx

--- src/components/Footer-section/Footer.jsx
+++ src/components/Footer-section/Footer.jsx
@@ -5,19 +5,13 @@ const Footer = () => {
   return (
     <div>
        <footer className="footer bg__nimbus text-white pb-5">
-         <div className="container">
+         <div className="container pr-5">
            <div className="row">
              <div className="col-md-4">
                <h2 className="header__poppins">Connect with me:</h2>
                <a href="https://github.com/calmcat1" target="_blank" className="font__poppins">GitHub</a><br></br>
                <a href="https://www.linkedin.com/in/lee-gitonga-3a5921242/" target="_blank" className="font__poppins">LinkedIn</a><br></br>
                <a href="mailto:tarafikicom@gmail.com" className="font__poppins">Email</a><br></br>
-             </div>
-             <div className="col-md-4">
-               <h1></h1>
-             </div>
-             <div className="col-md-4">
-               <h1></h1>
              </div>
            </div>
          </div>
